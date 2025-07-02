12dd71cdb4da541058601fd1a7d60e0ba1e8ac33
Micheal-18 <michaeleleke259@gmail.com>
2025-07-02 20:16:33 +0100

    simple navbar logo changed

--- src/components/SimpleNavbar.jsx
+++ src/components/SimpleNavbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../assets/rml.png'; // Adjust the path to your logo image
+
 
 const simpleNavbar = () => {
     return (
-        <div className='fixed flex mx-auto w-full justify-between items-center bg-black opacity-90 z-50'>
-                <a href="#"><img src={logo} alt='logo' className='w-10 h-10 rounded-full' /></a>
+        <div className='fixed flex mx-auto w-full justify-between items-center bg-black p-4 opacity-90 z-50'>
+                <Link to="/"><img src={logo} alt='logo' className='w-10 h-10 rounded-full' /></Link>
 
-                <Link to="/"> <button className='text-white text-center p-1 rounded-md  border border-white border-solid ml-2 bg-gradient-to-r from-black via-yellow to-yellow-500 transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-black text-lg'>
+                <Link to="/"> <button className='text-white text-center p-1 rounded-md  border border-white border-solid bg-gradient-to-r from-black via-yellow to-yellow-500 transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-black text-lg'>
                     ← Home
                 </button></Link>
         </div>
