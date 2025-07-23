5ebba6d8a546919b5403f2da7837a9d8686ce27e
Micheal-18 <michaeleleke259@gmail.com>
2025-07-23 14:15:56 +0100

    change

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === "production" && process.env.GITHUB_ACTIONS ? 
-  "/Real_michael/" //Gitpgh
-  : "/" //vercel
-
+  base: "/"
 })
