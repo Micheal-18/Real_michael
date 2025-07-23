09fb807cc42987bc6579fc36c59c4a1f28159468
Micheal-18 <michaeleleke259@gmail.com>
2025-07-23 15:16:22 +0100

    services

--- src/routes/Services.jsx
+++ src/routes/Services.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import img3 from "../assets/port.png"
 import img4 from "../assets/food.png"
 import img5 from "../assets/ecom.png"
+import img6 from  "../assets/eshop.png"
 // import { useInView } from 'react-intersection-observer'
 import { FaArrowRight } from 'react-icons/fa'
 import RevealOnScroll from '../components/RevealOnScroll';
@@ -52,7 +53,7 @@ const Services = () => {
 
             <div  className='p-6 rounded-xl border border-white/10 hover:-translate-y-1  hover:border-yellow-500/30 rgba(255, 255, 59, 0.1)] transition'>
               <h3 className='text-xl font-bold mb-2'>
-                E-Commerce Site
+                Landing Page
               </h3>
               <img className=' flex mx-auto rounded-sm justify-center items-center w-[80%] mb-6' src={img5} alt='portfolio' />
               <div className='flex flex-wrap gap-2 mb-4' n>
@@ -62,6 +63,20 @@ const Services = () => {
               </div>
               <div className='flex justify-between items-center my-2'>
                 <a href='https://micheal-18.github.io/Nexcent/' className='text-yellow-500 hover:text-yellow-300 transition-colors flex items-center gap-2'>View Project <FaArrowRight />  </a>
+              </div>
+            </div>
+            <div  className='p-6 rounded-xl border border-white/10 hover:-translate-y-1  hover:border-yellow-500/30 rgba(255, 255, 59, 0.1)] transition'>
+              <h3 className='text-xl font-bold mb-2'>
+                E-Commerce Site
+              </h3>
+              <img className=' flex mx-auto rounded-sm justify-center items-center w-[80%] mb-6' src={img6} alt='portfolio' />
+              <div className='flex flex-wrap gap-2 mb-4' n>
+                {["Reactjs","TailwindCss"].map((tech, key) => (
+                  <span key={key} className='bg-yellow-500 text-white py-1 px-3 rounded-full text-sm hover:bg-black hover:shadow-[0 2px 8px rgba(255, 255, 59, 0.1)] transition-all'>{tech}</span>
+                ))}
+              </div>
+              <div className='flex justify-between items-center my-2'>
+                <a href='https://eshop-tau-five.vercel.app' className='text-yellow-500 hover:text-yellow-300 transition-colors flex items-center gap-2'>View Project <FaArrowRight />  </a>
               </div>
             </div>
           </div>
