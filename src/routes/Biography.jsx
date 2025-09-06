eef5c19985677e78ccfaca59cf325e3adb3f06e0
Micheal-18 <michaeleleke259@gmail.com>
2025-09-06 21:18:33 +0100

    bio fade

--- src/routes/Biography.jsx
+++ src/routes/Biography.jsx
@@ -55,14 +55,14 @@ const Biography = () => {
           </div>
         </div>
         <div className='grid grid-cols-1 text-left md:grid-cols-2 gap-6 mt-8'>
-          <div data-aos="slide-left" className='p-6 rounded-xl border-yellow-500/10   border hover:-translate-y-1 transition-all '>
+          <div data-aos="fade-up" className='p-6 rounded-xl border-yellow-500/10   border hover:-translate-y-1 transition-all '>
             <h3 className='text-xl font-bold mb-4'>🎓 Education</h3>
             <ul className='list-disc list-inside text-gray-300 space-y-2 '>
               <li><strong>Undergraduate in Electrical Engineering</strong> University of Nigeria Nsukka (2021-2026) </li>
               <li>Relevant Coursework: Web Development </li>
             </ul>
           </div>
-          <div data-aos="slide-right" className='p-6 rounded-xl border-yellow-500/10   border hover:-translate-y-1 transition-all'>
+          <div data-aos="fade-up" className='p-6 rounded-xl border-yellow-500/10   border hover:-translate-y-1 transition-all'>
             <h3 className='text-xl font-bold mb-4'>💼 Work Experience</h3>
             <div className='space-y-4 text-gray-300'>
               <div>
